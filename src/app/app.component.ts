a43a96d743c5b4bd0b22a9957799efc5f820a780
eguchi-4696 <haxzf26261@yahoo.co.jp>
2022-05-17 15:24:04 +0900

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  template: `
+  <h1>{{ title }}</h1>
+<nav>
+  <a routerLink="/home">home</a>
+</nav>
+<router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {
   title = '家計簿アプリ';
